db91a5226846583d4ebd6860caaee8656025d801
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-04 14:17:05 +0530

    add

---
+++ operators-demo/src/com/operators/ternary/TernaryOperator.java
@@ -0,0 +1,17 @@
+package com.operators.ternary;
+import java.util.Scanner;
+public class TernaryOperator {
+    //pseudocode:
+    //accept the no as input.
+    //check if no is less than 0
+    //print negative no
+    //else print positive no
+    public static void main(String[] args) {
+        System.out.println("Enter the value:");
+        Scanner sc = new Scanner(System.in);
+        int input = Integer.parseInt(sc.nextLine());
+
+        //ternary operator represent
+        System.out.println((input < 0) ? "negative number" :"positive number");
+    }
+}
