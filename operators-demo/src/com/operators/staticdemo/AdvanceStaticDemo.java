56d4f932862e28e296aca8fb7347ed0d48113d31
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-11 00:39:29 +0530

    Add

---
+++ operators-demo/src/com/operators/staticdemo/AdvanceStaticDemo.java
@@ -0,0 +1,23 @@
+package com.operators.staticdemo;
+import java.util.Scanner;
+public class AdvanceStaticDemo {
+     static int staticNo;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        staticNo = sc.nextInt();
+
+        AdvanceStaticDemo a = new AdvanceStaticDemo();
+        a.printNo();
+
+        AdvanceStaticDemo a1 = new AdvanceStaticDemo();
+        a1.printNo();
+
+       // a.staticNo = 10;
+        //a1.printNo();
+    }
+    public void printNo(){
+        System.out.println("static number = " + staticNo);
+    }
+}
