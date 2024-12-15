f25e2ab51736829df21d4256a74a1de1d7683d43
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-15 15:36:30 +0530

    condition statement update

---
+++ core-java-demo/src/com/practice/conditionalstatement/IfDemo.java
@@ -0,0 +1,17 @@
+package src.com.practice.conditionalstatement;
+import java.util.Scanner;
+public class IfDemo {
+    //pseudocode
+    //if statement example
+    //enter the age and check
+    public static void main(String[] args) {
+        //System.out.println("Enter your age");
+        //Scanner sc = new Scanner(System.in);
+        //int age = sc.nextInt();
+        int age = 20;
+        //checking the age
+        if(age>18){
+            System.out.println("age is greater than 18");
+        }
+    }
+}
---
+++ core-java-demo/src/com/practice/conditionalstatement/IfElseDemo.java
@@ -0,0 +1,17 @@
+package src.com.practice.conditionalstatement;
+
+public class IfElseDemo {
+    //pseudocode
+    //if - else example
+    //enter number
+    //check then no even or odd.
+    public static void main(String[] args) {
+        int no = 187;
+        if(no % 2 == 0){
+            System.out.println("number is even");
+        }
+        else {
+            System.out.println("number is odd");
+        }
+    }
+}
