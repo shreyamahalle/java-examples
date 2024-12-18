7d0c1149535e97616e9709d8d342b693260e687c
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-18 19:10:24 +0530

    add

---
+++ core-java-demo/src/com/practice/array/ArrayDemoPracticeTwo.java
@@ -0,0 +1,30 @@
+package src.com.practice.array;
+
+public class ArrayDemoPracticeTwo { public static void main(String[] args) {
+    int marks[] = new int[5];
+
+    System.out.println("Size of array is: " + marks.length);
+    //while loop...................................
+    System.out.println("-----------while loop-----------");
+    int position = 0;
+    System.out.println("Demo of while loop:");
+    while (position < marks.length) {
+        System.out.println(marks[position]);
+        position++;
+    }
+    //for loop..
+    System.out.println("------------------for loop----------------------");
+    System.out.println("demo of for loop");
+    for(int index = 0; index < marks.length; index++){
+        System.out.println(marks[index]);
+    }
+    //for each loop
+    System.out.println("--------------for each loop--------------");
+    System.out.println("demo of for-each-loop");
+    for(int element : marks){
+        System.out.println(element);
+    }
+
+}
+}
+
