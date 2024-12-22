5388d10c54b120c915b8bd1055a561cb48c64eb3
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-22 21:15:28 +0530

    updated

---
+++ core-java-demo/src/com/practice/pattern/StarPatternForLoop.java
@@ -0,0 +1,16 @@
+package src.com.practice.pattern;
+
+public class StarPatternForLoop {
+    private static int pattern_size = 5;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < pattern_size; i++) {
+            for (int index = 0; index <= i; index++) {
+                System.out.print("*\t");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
