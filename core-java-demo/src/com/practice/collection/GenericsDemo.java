90d3d4a516cd8eeb4159ce93ae8982734ab8f84e
shreya mahalle <shreyamahalle2000@gmail.com>
2024-12-26 20:15:12 +0530

    generics

---
+++ core-java-demo/src/com/practice/collection/GenericsDemo.java
@@ -0,0 +1,22 @@
+package src.com.practice.collection;
+
+import java.util.ArrayList;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        ArrayList<String> cityGenericsList = new ArrayList<>();
+        cityGenericsList.add("pune");
+        cityGenericsList.add("mumbai");
+        cityGenericsList.add("delhi");
+        cityGenericsList.add("amravati");
+        //cityGenericsList.add(true);
+        System.out.println("ArrayList <String> " + cityGenericsList);
+
+        ArrayList<Integer> value = new ArrayList<>();
+        value.add(3);
+        value.add(665555);
+        value.add(456789);
+        System.out.println("ArrayList <Int>" + value);
+
+    }
+}
