13fd386ce5c5237ccf1611a6212fdb2c200fd719
shreya mahalle <shreyamahalle2000@gmail.com>
2025-01-02 18:29:48 +0530

    map

---
+++ core-java-demo/src/com/practice/collection/map/HashMapReDemo.java
@@ -0,0 +1,16 @@
+package src.com.practice.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapReDemo {
+    public static void main(String[] args) {
+        Map<Integer,String> cityMap = new HashMap<>();
+        cityMap.put(1,"Delhi");
+        cityMap.put(2,"Mumbai");
+        System.out.println(cityMap);
+
+
+
+}
+}
