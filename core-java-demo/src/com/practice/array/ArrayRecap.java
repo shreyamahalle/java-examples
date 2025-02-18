3cf6fde6f2deab4001621f5a7670d58cd8e7669f
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-18 12:45:12 +0530

    added new example

--- core-java-demo/src/com/practice/array/ArrayRecap.java
+++ core-java-demo/src/com/practice/array/ArrayRecap.java
@@ -1,5 +1,13 @@
 package src.com.practice.array;
 
 public class ArrayRecap {
+    public static void main(String[] args) {
+    int array[] = {1,2,3,4,5,6,7,8,9};
+    //int n = array.length;
+    //System.out.println(array.length);
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
 
+    }
 }
