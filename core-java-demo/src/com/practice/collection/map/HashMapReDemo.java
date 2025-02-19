856415cc0a16ba19060b2f98b455c2f3ae2ca445
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-19 21:49:12 +0530

    new added

--- core-java-demo/src/com/practice/collection/map/HashMapReDemo.java
+++ core-java-demo/src/com/practice/collection/map/HashMapReDemo.java
@@ -5,15 +5,14 @@ import java.util.Map;
 public class HashMapReDemo {
     public static void main(String[] args) {
         Map<Integer,String> cityMap = new HashMap<>();
+        Map<String,String> cities = new HashMap<>();
         cityMap.put(1,"Delhi");
         cityMap.put(2,"Mumbai");
         cityMap.put(3,"Delhi");
         cityMap.put(4,"Nagpur");
-        cityMap.put(5,"Nagpur");
-        cityMap.put(6,"Nagpur");
-        cityMap.put(7,"Nagpur");
-        cityMap.put(8,"Nagpur");
+        cities.put("shreya","shreya");
         System.out.println("Map " + cityMap);
         System.out.println("=========================================");
-}
+        System.out.println("cities " + cities);
+    }
 }
