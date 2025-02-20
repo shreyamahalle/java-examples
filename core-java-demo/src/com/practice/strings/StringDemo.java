1c9b97d5366701a277c0482d0a1f6b3431f859da
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-21 00:31:36 +0530

    added

--- core-java-demo/src/com/practice/strings/StringDemo.java
+++ core-java-demo/src/com/practice/strings/StringDemo.java
@@ -1,4 +1,10 @@
 package src.com.practice.strings;
 
 public class StringDemo {
+    public static void main(String[] args) {
+        String str = "India";
+        String str1 = new String("India");
+        System.out.println("String literal " + str );
+        System.out.println("string object " + str1);
+    }
 }
