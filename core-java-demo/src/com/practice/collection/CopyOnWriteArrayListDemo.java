c2c7d9c6fc42e957f07363b7201965bedb6503f6
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-20 23:42:24 +0530

    new changes

--- core-java-demo/src/com/practice/collection/CopyOnWriteArrayListDemo.java
+++ core-java-demo/src/com/practice/collection/CopyOnWriteArrayListDemo.java
@@ -2,10 +2,12 @@ package src.com.practice.collection;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CopyOnWriteArrayListDemo {
     public static void main(String[] args) {
-        List<Integer> integerList = List.of(1,4,2,6,875,6,45,4545,4545,45,667,7);
+        CopyOnWriteArrayList<Integer> integerList = new CopyOnWriteArrayList<>();
+        integerList.addAll(List.of(1,4,2,6,875,6,45,4545,4545,45,667,7));
 
         Iterator<Integer> integerIterator  = integerList.iterator();
 
