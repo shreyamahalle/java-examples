8e7097339935f5c7e685e7ae5c8e422ed88a630e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-21 14:53:24 +0530

    string

--- core-java-demo/src/com/practice/strings/StringDemo.java
+++ core-java-demo/src/com/practice/strings/StringDemo.java
@@ -2,9 +2,24 @@ package src.com.practice.strings;
 
 public class StringDemo {
     public static void main(String[] args) {
-        String str = "India";
-        String str1 = new String("India");
+
+        String str = "India";//string literal
+        String str1 = new String("India");//string object
         System.out.println("String literal " + str );
-        System.out.println("string object " + str1);
+        System.out.println("String object " + str1);
+
+        //immutable object trying to change
+        str1 = new String("India132342");
+        System.out.println(str1);
+        System.out.println("--------------------------------");
+
+        //mutable object
+        StringBuilder wish = new StringBuilder(" hi hello ");
+        System.out.println("current object " + wish);
+        wish.append(" happy birthday ");
+        System.out.println("current object " + wish);
+        wish = new StringBuilder("new data");
+        System.out.println("current object " + wish );
+
     }
 }
