9fed14ba696988a5f457cdc1c91242119a389e66
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-21 14:59:05 +0530

    added

--- core-java-demo/src/com/practice/strings/StringDemo.java
+++ core-java-demo/src/com/practice/strings/StringDemo.java
@@ -13,13 +13,22 @@ public class StringDemo {
         System.out.println(str1);
         System.out.println("--------------------------------");
 
-        //mutable object
+        //mutable object - for non-multi-threading application
         StringBuilder wish = new StringBuilder(" hi hello ");
         System.out.println("current object " + wish);
         wish.append(" happy birthday ");
         System.out.println("current object " + wish);
         wish = new StringBuilder("new data");
         System.out.println("current object " + wish );
+        System.out.println("--------------------------------------");
+
+        //mutable object for multi-threading application
+        StringBuffer stringBuffer = new StringBuffer("hi");
+        System.out.println("current string buffer " + stringBuffer);
+        stringBuffer.append("all good?");
+        System.out.println("string " + stringBuffer);
+        stringBuffer = new StringBuffer("ssssssssssss");
+        System.out.println("current string buffer " + stringBuffer);
 
     }
 }
