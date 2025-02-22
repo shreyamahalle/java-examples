dd55f55fde5529343103945a12ece829734bb3d7
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-22 15:03:08 +0530

    added

--- core-java-demo/src/com/practice/staticimport/StaticImport.java
+++ core-java-demo/src/com/practice/staticimport/StaticImport.java
@@ -1,9 +1,9 @@
 package src.com.practice.staticimport;
-
-import src.com.practice.staticimport.service.HelloService;
+import static src.com.practice.staticimport.service.HelloService.sayHello;
 
 public class StaticImport {
     public static void main(String[] args) {
-        HelloService.sayHello("Shreya");
+        sayHello("Shreya");
+        sayHello("shreya", "amt");
     }
 }
--- core-java-demo/src/com/practice/staticimport/service/HelloService.java
+++ core-java-demo/src/com/practice/staticimport/service/HelloService.java
@@ -4,4 +4,9 @@ public class HelloService {
     public static void sayHello(String name){
         System.out.println("Hello " + (name.isEmpty()? "world ": name +  "!!" ));
     }
-}
+//    public static void sayHello(String...name){
+//        for (String name: name){
+//            System.out.println("Hello " + name);
+//        }
+    }
+
