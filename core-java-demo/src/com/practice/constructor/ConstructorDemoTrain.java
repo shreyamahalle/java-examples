0cc09413427d82d938ca50803836a8bf4ee41541
shreya mahalle <shreyamahalle2000@gmail.com>
2025-02-22 22:26:18 +0530

    constructor

---
+++ core-java-demo/src/com/practice/constructor/ConstructorDemoTrain.java
@@ -0,0 +1,41 @@
+package src.com.practice.constructor;
+
+public class ConstructorDemoTrain {
+    private int trainId;
+    private String trainName;
+
+    public ConstructorDemoTrain(){
+
+    }
+    public ConstructorDemoTrain(int id,String trainName){
+        this.trainId = id;
+        this.trainName = trainName;
+
+    }
+
+    public int getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(int trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainName() {
+        return trainName;
+    }
+
+    @Override
+    public String toString() {
+        return "ConstructorDemoTrain{" +
+                "trainId=" + trainId +
+                ", trainName='" + trainName + '\'' +
+                '}';
+    }
+
+    public void setTrainName(String trainName) {
+        this.trainName = trainName;
+
+
+    }
+}
