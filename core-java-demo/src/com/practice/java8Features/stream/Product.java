5a829d89d253fa650f0a2c269e9ca3a5319c52bf
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-05 23:55:20 +0530

    stream example added

--- core-java-demo/src/com/practice/java8Features/stream/Product.java
+++ core-java-demo/src/com/practice/java8Features/stream/Product.java
@@ -4,10 +4,19 @@ public class Product {
     int id;
     String name;
     float price;
-    Product(int id,String name,float price){
+
+    Product(int id, String name, float price) {
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public Float price() {
+        return 0f;
+    }
+
+    public Integer id() {
+        return 0;
     }
 }
 
