3d7acf0ce648499bdc94c2823bec6d0700827acc
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-05 22:07:44 +0530

    example added

--- core-java-demo/src/com/practice/java8Features/functionInterface/additionalExamples/GreetingsExample.java
+++ core-java-demo/src/com/practice/java8Features/functionInterface/additionalExamples/GreetingsExample.java
@@ -4,9 +4,15 @@ public class GreetingsExample {
     public static void main(String[] args) {
         Greetings greetings = () -> System.out.println("Hello ");
         greetings.sayHello();
+
+        GreetingsToAll greetingsToAll = (name) -> System.out.println("Hi " + name );
+        greetingsToAll.SayHelloToAll("pune");
     }
 }
 
 interface Greetings{
     void sayHello();
+}
+interface  GreetingsToAll{
+    void SayHelloToAll(String name);
 }
