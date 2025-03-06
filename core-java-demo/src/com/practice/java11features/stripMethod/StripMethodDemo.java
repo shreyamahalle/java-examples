1d3a604e78eb8f46a341f34b140fc143946e3509
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-06 21:40:27 +0530

    strip mothod example added

---
+++ core-java-demo/src/com/practice/java11features/stripMethod/StripMethodDemo.java
@@ -0,0 +1,8 @@
+package src.com.practice.java11features.stripMethod;
+
+public class StripMethodDemo {
+    public static void main(String[] args) {
+        var nextWhiteSpaceString = "\n\t\u2008hello how are you\u2005";
+        System.out.println("our string " + nextWhiteSpaceString);
+    }
+}
