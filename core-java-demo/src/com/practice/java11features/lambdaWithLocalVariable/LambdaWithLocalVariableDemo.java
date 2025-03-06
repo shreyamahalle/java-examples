490cd7e72efd715c181489920aa8c1dd65030d81
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-07 00:43:40 +0530

    random number generated examples are updated

--- core-java-demo/src/com/practice/java11features/lambdaWithLocalVariable/LambdaWithLocalVariableDemo.java
+++ core-java-demo/src/com/practice/java11features/lambdaWithLocalVariable/LambdaWithLocalVariableDemo.java
@@ -3,8 +3,11 @@ package src.com.practice.java11features.lambdaWithLocalVariable;
 public class LambdaWithLocalVariableDemo {
     public static void main(String[] args) {
         Interest1 interest1 = (principalAmount,  interestRatee) ->{
-
-            return principalAmount *(interestRatee / 100) * 10;
+            int year = 1;
+            return principalAmount *(interestRatee / 100) * year;
         };
+
+        double totalInterest = interest1.calculate(32534,5.65f);
+        System.out.println("Total interest " + totalInterest);
     }
 }
--- core-java-demo/src/com/practice/java8Features/lambdaExpression/LambdaExpression1.java
+++ core-java-demo/src/com/practice/java8Features/lambdaExpression/LambdaExpression1.java
@@ -1,14 +1,14 @@
-package src.com.practice.java8Features.lambdaExpression;
-
-public class LambdaExpression1 {
-    public static void main(String[] args) {
-
-        Interest interest = (int principalAmt, float interestRate) -> principalAmt * (interestRate /100);
-
-        int principalAmt = 1000;
-        float interestRate = 8.5f;
-        double simpleInterest = interest.calculate(principalAmt, interestRate);
-        System.out.println("Simple Interest: " + simpleInterest);
-    }
-}
-
+//package src.com.practice.java8Features.lambdaExpression;
+//
+//public class LambdaExpression1 {
+//    public static void main(String[] args) {
+//
+//        Interest interest = (int principalAmt, float interestRate) -> principalAmt * (interestRate /100);
+//
+//        int principalAmt = 1000;
+//        float interestRate = 8.5f;
+//        double simpleInterest = interest.calculate(principalAmt, interestRate);
+//        System.out.println("Simple Interest: " + simpleInterest);
+//    }
+//}
+//
