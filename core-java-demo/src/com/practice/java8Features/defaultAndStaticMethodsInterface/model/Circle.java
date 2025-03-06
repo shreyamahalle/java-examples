ee37958f43a77499f859c37c605153e7d1720cbb
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-06 15:48:21 +0530

    practice example added

--- core-java-demo/src/com/practice/java8Features/defaultAndStaticMethodsInterface/model/Circle.java
+++ core-java-demo/src/com/practice/java8Features/defaultAndStaticMethodsInterface/model/Circle.java
@@ -4,6 +4,9 @@ public class Circle implements Shape{
 
     @Override
     public void draw() {
-
+        System.out.println("Circle drawn");
+    }
+    public void print(){
+        System.out.println("Circle is painted with base color...");
     }
 }
