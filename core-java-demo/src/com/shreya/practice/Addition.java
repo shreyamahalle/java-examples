dbd1e304dc41fdac47643c8323489c759a3f4a26
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-24 23:51:29 +0530

    practice

--- core-java-demo/src/com/shreya/practice/Addition.java
+++ core-java-demo/src/com/shreya/practice/Addition.java
@@ -1,10 +1,19 @@
 package src.com.shreya.practice;
 
+import java.util.Scanner;
+
 public class Addition {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println(c);
+//        int a = 10;
+//        int b = 20;
+//        int c = a + b;
+//        System.out.println(c);
+        System.out.println("Enter number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+
+        System.out.println("addition " + sum);
     }
 }
