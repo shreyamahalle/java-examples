9aa765c3fb520f14360f75823d747d4b835dffc5
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-25 16:30:01 +0530

    practice example added

--- core-java-demo/src/com/shreya/number/OddEvenDemo.java
+++ core-java-demo/src/com/shreya/number/OddEvenDemo.java
@@ -1,8 +1,15 @@
 package src.com.shreya.number;
 
-public class OddEven {
+import java.util.Scanner;
+
+public class OddEvenDemo {
+
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int no = 219;
+        System.out.println("Enter number");
+        int no = sc.nextInt();
+
         if (no % 2 == 0) {
             System.out.println("no is even");
         } else {
--- core-java-demo/src/com/shreya/practice/RelationalOperatorDemo.java
+++ core-java-demo/src/com/shreya/practice/RelationalOperatorDemo.java
@@ -1,6 +1,6 @@
 package src.com.shreya.practice;
-
 public class RelationalOperatorDemo {
+
     public static void main(String[] args) {
         int a = 1241;
         int b = 1242;
@@ -9,7 +9,6 @@ public class RelationalOperatorDemo {
             System.out.println("a is greater than b");
         }else{
             System.out.println("a is not greater than b");
-
         }
         if(a == b){
             System.out.println("value of a is exact equal to value of b");
