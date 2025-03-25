930c2f5c49220eaab6d1d0c26198943520289061
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-25 18:07:29 +0530

    changes added

--- operators-demo/src/com/operators/logicalOperator/NumberTypeDemo.java
+++ operators-demo/src/com/operators/logicalOperator/NumberTypeDemo.java
@@ -1,8 +1,8 @@
 package com.operators.logicalOperator;
-
 import java.util.Scanner;
 
 public class NumberTypeDemo {
+
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -12,10 +12,13 @@ public class NumberTypeDemo {
             System.out.println("number is golden");
         }
         else if((no % 2 == 0) && (no <= 100)){
-                System.out.println(" number is golden");
+                System.out.println(" number is silver");
             }
-        else if((no % 2 == 0) && (no <= 100)){
-            System.out.println(" number is golden");
+        else if((no % 2 != 0) && (no > 100)){
+            System.out.println(" number is platinum");
+        }
+        else if((no % 2 != 0) && (no <= 100)){
+            System.out.println(" number is titanium");
         }
       }
     }
