633e916689c6728e476b68fba473d6401b1d0908
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-25 13:29:57 +0530

    operator example

---
+++ operators-demo/src/com/operators/arithmetic/operators/EqualToOperator.java
@@ -0,0 +1,14 @@
+package com.operators.arithmetic.operators;
+
+public class EqualToOperator {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 23;
+
+        if(a != b) {
+            System.out.println("a is not equal to b");
+        }else {
+        System.out.println("a is  equal to b");
+    }
+    }
+}
