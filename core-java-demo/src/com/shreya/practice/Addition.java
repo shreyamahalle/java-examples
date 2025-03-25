04a5d35ddcd3bc1deb432a7e82eb58d889288274
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-25 10:42:48 +0530

    added

--- core-java-demo/src/com/shreya/practice/Addition.java
+++ core-java-demo/src/com/shreya/practice/Addition.java
@@ -15,5 +15,13 @@ public class Addition {
         int sum = a + b ;
 
         System.out.println("addition " + sum);
+        int mul = a * b ;
+        System.out.println("multiplication " + mul);
+
+        int div = a / b ;
+        System.out.println("division " + div);
+
+        int sub = a - b ;
+        System.out.println("substation  " + sub);
     }
 }
