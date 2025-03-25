5ffc003b826126d179c79f63ab4b29f54425a526
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-25 11:23:27 +0530

    added

--- core-java-demo/src/com/shreya/practice/RelationalOperatorDemo.java
+++ core-java-demo/src/com/shreya/practice/RelationalOperatorDemo.java
@@ -1,5 +1,22 @@
 package src.com.shreya.practice;
 
 public class RelationalOperatorDemo {
+    public static void main(String[] args) {
+        int a = 1241;
+        int b = 1241;
 
+        if(a < b){
+            System.out.println("a is greater than b");
+        }else{
+            System.out.println("a is not greater than b");
+
+        }
+
+        if(a == b){
+            System.out.println("value of a is exact equal to value of b");
+        }else{
+            System.out.println("value of a is not exact equal to value of b");
+        }
+
+    }
 }
