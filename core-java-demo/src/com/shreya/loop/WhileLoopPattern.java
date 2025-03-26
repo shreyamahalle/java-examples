7c0ecb86097f3da4ee1fa6e98bd4efd56b3c7067
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-26 23:36:24 +0530

    added

---
+++ core-java-demo/src/com/shreya/loop/WhileLoopPattern.java
@@ -0,0 +1,13 @@
+package src.com.shreya.loop;
+
+public class WhileLoopPattern {
+    //expected output
+    //*
+    //**
+    //***
+    //****
+    //*****
+    public static void main(String[] args) {
+
+    }
+}
