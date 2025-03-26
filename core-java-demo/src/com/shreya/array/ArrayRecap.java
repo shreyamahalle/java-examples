15cdf83f30c62c483bffe8e9a53173ce52083833
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-26 16:54:45 +0530

    added

--- core-java-demo/src/com/shreya/array/ArrayRecap.java
+++ core-java-demo/src/com/shreya/array/ArrayRecap.java
@@ -2,12 +2,9 @@ package src.com.shreya.array;
 
 public class ArrayRecap {
     public static void main(String[] args) {
-    int array[] = {1,2,3,4,5,6,7,8,9};
-    //int n = array.length;
-    //System.out.println(array.length);
-        for(int i=0; i<array.length; i++){
-            System.out.println(array[i]);
-        }
 
+    int[] array = {1,2,3,4,5,6,7,8,9};
+        for (int a : array)
+            System.out.println(a);
     }
 }
