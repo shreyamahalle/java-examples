5b8cf08b14df8280ecf1c777447306a56e7e8bad
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-26 14:17:18 +0530

    practice example added

--- operators-demo/src/com/operators/bitwise/AndDemo.java
+++ operators-demo/src/com/operators/bitwise/AndDemo.java
@@ -2,6 +2,11 @@ package com.operators.bitwise;
 
 public class AndDemo {
     public static void main(String[] args) {
+    int n1 = 8;
+    int n2 = 9;
 
+    int result = n1 & n2;
+
+        System.out.println("AND value is " + result);
     }
 }
--- operators-demo/src/com/operators/bitwise/OrDemo.java
+++ operators-demo/src/com/operators/bitwise/OrDemo.java
@@ -2,6 +2,10 @@ package com.operators.bitwise;
 
 public class OrDemo {
     public static void main(String[] args) {
+        int n  = 2;
+        int n1 = 7;
 
+        int result = n | n1;
+        System.out.println("or value is " + result);
     }
 }
