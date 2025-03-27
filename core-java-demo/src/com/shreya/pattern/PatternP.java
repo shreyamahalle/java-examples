a386ec1ca9f08ea66e1e927528699e992b7af7e5
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-27 11:07:40 +0530

    changes added

--- core-java-demo/src/com/shreya/pattern/PatternP.java
+++ core-java-demo/src/com/shreya/pattern/PatternP.java
@@ -4,11 +4,10 @@ public class PatternP {
     public static void main(String[] args) {
         int col = 0;
         while(col < 5) {
-            //System.out.println(col);
 
             int row = 0;
             while(row <=  col) {
-                System.out.print("* \t");
+                System.out.print( col + " \t");
                 row++;
             }
             System.out.println();
