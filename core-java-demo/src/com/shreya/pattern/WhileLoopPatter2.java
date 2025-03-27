4508712368247df39e58257e99a785d8154b4994
shreya mahalle <shreyamahalle2000@gmail.com>
2025-03-27 11:56:09 +0530

    changes added

---
+++ core-java-demo/src/com/shreya/pattern/WhileLoopPatter2.java
@@ -0,0 +1,18 @@
+package src.com.shreya.pattern;
+
+public class WhileLoopPatter2
+{
+    public static void main(String[] args) {
+        int row = 0;
+        while (row <=5 ){
+            System.out.println(row);
+
+        int index = 0;
+        while (index < 10){
+            System.out.print(index + " \t");
+            index++;
+        }
+            row++;
+        }
+    }
+}
