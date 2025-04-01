1a778a42a7c942145bd2c25edd3afa39c95669e5
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-01 19:24:04 +0530

    servlet project added

--- servlet-project/src/main/java/com/shreya/servlet/service/HelloService.java
+++ servlet-project/src/main/java/com/shreya/servlet/service/HelloService.java
@@ -1,7 +1,7 @@
 package com.shreya.servlet.service;
 
-public class HelloService
-{
+public class HelloService {
+
     public String greet(String name) {
         return "hello " + (name.isBlank() ? "world!!" : name);
     }
