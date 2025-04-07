b9898e5b4dd51f98d315efeb27fb5dfc3e2a9a6e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-07 11:31:07 +0530

    changes added

--- core-java-demo/src/com/shreya/array/Demo.java
+++ core-java-demo/src/com/shreya/array/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
         int[] arr = {2,3,4,5,2,1,3,43};
         for(int a : arr){
             System.out.println(a);
+
+        for (int i = 0; i<= arr.length; i++)  {
+            System.out.println(arr);
         }
-    }
+     }
+   }
 }
