f0bce1b2e8bcb294d7eac08a9029dc90e8100baa
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-07 11:28:35 +0530

    practice example addedd

---
+++ core-java-demo/src/com/shreya/array/Demo.java
@@ -0,0 +1,10 @@
+package src.com.shreya.array;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,2,1,3,43};
+        for(int a : arr){
+            System.out.println(a);
+        }
+    }
+}
