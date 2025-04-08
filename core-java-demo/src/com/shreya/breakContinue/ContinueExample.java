36dc845b215e5e5fbb3e2694955f098669a19508
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-08 13:25:17 +0530

    changes added

--- core-java-demo/src/com/shreya/breakContinue/ContinueExample.java
+++
@@ -1,7 +0,0 @@
-package src.com.shreya.breakContinue;
-
-public class ContinueExample {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ core-java-demo/src/com/shreya/breakContinue/ForLoopContinueExample.java
@@ -0,0 +1,12 @@
+package src.com.shreya.breakContinue;
+
+public class ForLoopContinueExample {
+    public static void main(String[] args) {
+        for (int i=0; i<=5; i++){
+            if (i == 3){
+                continue;//skip the reat of the loop when i = 3.
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
