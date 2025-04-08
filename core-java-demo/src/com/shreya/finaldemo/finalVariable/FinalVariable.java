8bcb38abfe52709ab62e5a138c1f2fefe840be2d
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-08 15:51:51 +0530

    practice

---
+++ core-java-demo/src/com/shreya/finaldemo/finalVariable/FinalVariable.java
@@ -0,0 +1,10 @@
+package src.com.shreya.finaldemo.finalVariable;
+
+public class FinalVariable {
+    public static void main(String[] args) {
+        final int no = 10;
+        //no = 20; // Error: cannot assign a value to final variable no
+        System.out.println(no);
+
+    }
+}
