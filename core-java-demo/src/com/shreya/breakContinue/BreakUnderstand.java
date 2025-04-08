d5c25c3af94ccdfbf20474e64088b40a257052d3
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-08 12:49:18 +0530

    changes added

--- core-java-demo/src/com/shreya/breakContinue/BreakUnderstand.java
+++ core-java-demo/src/com/shreya/breakContinue/BreakUnderstand.java
@@ -1,7 +1,21 @@
 package src.com.shreya.breakContinue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BreakUnderstand {
     public static void main(String[] args) {
-
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Kivi");
+        for (String item : fruits){
+            if (item.equals("stop")){
+                System.out.println("breaking " + item);
+                break;
+            }
+            System.out.println(item);
+        }
     }
 }
---
+++ core-java-demo/src/com/shreya/breakContinue/ContinueExample.java
@@ -0,0 +1,7 @@
+package src.com.shreya.breakContinue;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+
+    }
+}
