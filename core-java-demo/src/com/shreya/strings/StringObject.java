446f75b42242ca2aa7e5402534fe628a71c2428f
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-11 16:49:39 +0530

    changes added

---
+++ core-java-demo/src/com/shreya/strings/StringObject.java
@@ -0,0 +1,10 @@
+package src.com.shreya.strings;
+
+//string object created in heap.
+
+public class StringObject {
+    public static void main(String[] args) {
+        String str = new String("shreya");
+        System.out.println(str);
+    }
+}
