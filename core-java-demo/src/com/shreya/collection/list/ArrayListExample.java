2abbf7200ca5022e4691297422ee16663d42f8e4
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-18 13:09:29 +0530

    ArrayList

--- core-java-demo/src/com/shreya/collection/list/ArrayListExample.java
+++ core-java-demo/src/com/shreya/collection/list/ArrayListExample.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListExample {
+
     public static void main(String[] args) {
 
         List<String> cities = new ArrayList<>();
-
+        //add() method
         cities.add("Pune");
         cities.add("Mumbai");
         cities.add("Delhi");
@@ -15,6 +16,17 @@ public class ArrayListExample {
         cities.add("Dadar");
 
         System.out.println("Cities List is " + cities);
+        System.out.println("--------------------------------------");
+        //get() method
+        String mumbaiCity = cities.get(1);
+        System.out.println("Accessed Element " + mumbaiCity);
+        System.out.println("--------------------------------------");
+
+        //remove () method
+        String removedCity = cities.remove(1);
+        System.out.println("Removed Element " + removedCity);
+        System.out.println("Remaining  cities " + cities);
+        System.out.println("--------------------------------------");
 
     }
 
