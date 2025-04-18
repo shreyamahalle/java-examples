aa6eeef26c034083249532ed858f1e518b84269d
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-18 23:00:15 +0530

    changes added

---
+++ core-java/src/com/shreya/practice/constructor/paremeterizedConstructor/Car.java
@@ -0,0 +1,16 @@
+package com.shreya.practice.constructor.paremeterizedConstructor;
+
+public class Car {
+    int modelYear;
+    String modelName;
+
+    public Car(int modelYear,String modelName){
+       this.modelYear = modelYear;
+       this.modelName = modelName;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car(2000,"honda");
+        System.out.println(car.modelYear + " " + car.modelName);
+    }
+}
