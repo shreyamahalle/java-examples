17b77ad430a714dbb6e23b547d6b8e0c9faa5d8f
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 15:13:34 +0530

    changes added

---
+++ core-java/src/com/shreya/practice/strings/stringNewKeyword/UsingNewKeyword.java
@@ -0,0 +1,9 @@
+package com.shreya.practice.strings.stringNewKeyword;
+
+public class UsingNewKeyword {
+    public static void main(String[] args) {
+        String name = new String("Welcome");
+        System.out.println(name);
+
+    }
+}
