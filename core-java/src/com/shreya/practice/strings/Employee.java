7f722e523a346a8665ddd4d9b7ca914dfd253b01
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 20:38:49 +0530

    changes added

--- core-java/src/com/shreya/practice/strings/Employee.java
+++ core-java/src/com/shreya/practice/strings/Employee.java
@@ -1,7 +1,17 @@
 package com.shreya.practice.strings;
-//example to create immutable class
-public class Employee {
+//example to create immutable class //immutable means once we create we can not change
+public final class Employee {
+
+    final String panCardNo;
+
+    public Employee(String panCardNo) {
+        this.panCardNo = panCardNo;
+    }
+    public  String getPanCardNo(){
+        return panCardNo;
+    }
     public static void main(String[] args) {
+        Employee employee = new Employee("PCV54");
 
     }
 }
