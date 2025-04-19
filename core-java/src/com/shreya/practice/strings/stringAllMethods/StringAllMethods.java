67b04b249499e74adbd8021e5ccbfbd96b77c0aa
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 16:46:11 +0530

    string methods

---
+++ core-java/src/com/shreya/practice/strings/stringAllMethods/StringAllMethods.java
@@ -0,0 +1,14 @@
+package com.shreya.practice.strings.stringAllMethods;
+
+public class StringAllMethods {
+    public static void main(String[] args) {
+
+        //toUpperCase() method
+        String s = "Ram";
+        System.out.println(s.toUpperCase());
+        //toLowerCase() method
+        System.out.println(s.toLowerCase());
+
+
+    }
+}
