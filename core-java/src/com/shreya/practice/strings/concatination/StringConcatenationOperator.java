cdf6b7cf4e9d13e65a2288c335b9e0799ada6715
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 16:11:24 +0530

    string

--- core-java/src/com/shreya/practice/strings/concatination/StringConcatenationOperator.java
+++ core-java/src/com/shreya/practice/strings/concatination/StringConcatenationOperator.java
@@ -1,4 +1,8 @@
 package com.shreya.practice.strings.concatination;
 
 public class StringConcatenationOperator {
+    public static void main(String[] args) {
+        String s = "shreya " + " shreya";
+        System.out.println("concatenation " + s);
+    }
 }
