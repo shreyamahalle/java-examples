eee5fe19da06a86ad6d1bf5bb63ff7afe07c3418
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 16:24:48 +0530

    changes

--- core-java/src/com/shreya/practice/strings/concatination/StringConcatMethod.java
+++ core-java/src/com/shreya/practice/strings/concatination/StringConcatMethod.java
@@ -2,6 +2,11 @@ package com.shreya.practice.strings.concatination;
 
 public class StringConcatMethod {
     public static void main(String[] args) {
+        String s1 = "ram";
+        String s2 = "shyam";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+        System.out.println(s1.concat(s2));
 
     }
 }
