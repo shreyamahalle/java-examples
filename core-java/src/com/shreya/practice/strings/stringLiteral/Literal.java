4a38ab2deeb45f610a68cd959ea72e704befe276
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-19 14:13:02 +0530

    changes added

---
+++ core-java/src/com/shreya/practice/strings/stringLiteral/Literal.java
@@ -0,0 +1,8 @@
+package com.shreya.practice.strings.stringLiteral;
+
+public class Literal {
+    public static void main(String[] args) {
+        String name = "Shreya";
+        System.out.println("name is " + name);
+    }
+}
