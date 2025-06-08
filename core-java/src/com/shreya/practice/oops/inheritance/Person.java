c1b1032e712fb2fedff06577d09b6c9464affb67
shreya mahalle <shreyamahalle2000@gmail.com>
2025-06-08 13:57:05 +0530

    Inheritance code added

--- core-java/src/com/shreya/practice/oops/inheritance/Person.java
+++ core-java/src/com/shreya/practice/oops/inheritance/Person.java
@@ -4,9 +4,6 @@ public class Person {
     String name;
     String address;
     String mobileNo;
-    public void show(){
-        System.out.println("Person Information: " + " name " + name + " address " + address + " mobileNo " + mobileNo);
-    }
 
     public static void main(String[] args) {
         Employee employee = new Employee();
@@ -19,13 +16,18 @@ public class Person {
         employee.show();
 
     }
+
+    public void show() {
+        System.out.println("Person Information: " + " name " + name + " address " + address + " mobileNo " + mobileNo);
+    }
 }
 
-class Employee extends Person{
+class Employee extends Person {
     String empId;
     String companyName;
+
     @Override
-    public void show(){
+    public void show() {
         super.show();
         System.out.println("Employee Information: " + " empId " + empId + " companyName " + companyName);
     }
