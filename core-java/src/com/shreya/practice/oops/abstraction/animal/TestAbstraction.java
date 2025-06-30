615c9b81b9fd3226430e11cb1cc3bfa14de5460a
shreya mahalle <shreyamahalle2000@gmail.com>
2025-06-30 19:23:45 +0530

    abstaction example added

---
+++ core-java/src/com/shreya/practice/oops/abstraction/animal/TestAbstraction.java
@@ -0,0 +1,22 @@
+package com.shreya.practice.oops.abstraction.animal;
+
+ abstract class Animal {
+     abstract void sound();   //Abstract method nobody
+
+     void sleep(){            // non-abstract method
+         System.out.println("Animal sleeping..");
+     }
+}
+class Dog extends  Animal{
+
+     void sound() {
+         System.out.println("Dog barks");
+    }
+}
+ public class TestAbstraction {
+     public static void main(String[] args) {
+         Animal obj = new Dog();
+         obj.sound();
+         obj.sleep();
+     }
+}
