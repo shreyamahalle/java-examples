4e24725cc58e2283800d9aaea652ab5f3ce69c18
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-01 15:04:36 +0530

    updated

---
+++ core-java/src/com/shreya/practice/finaldemo/finalVariable/ExampleDemo.java
@@ -0,0 +1,15 @@
+package com.shreya.practice.finaldemo.finalVariable;
+
+public class ExampleDemo {
+
+    final int speed = 80;
+
+    void setSpeed(){
+        System.out.println("speed is " + speed);
+    }
+
+    public static void main(String[] args) {
+        ExampleDemo exampleDemo = new ExampleDemo();
+        exampleDemo.setSpeed();
+    }
+}
