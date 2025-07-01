0dabe03638013732fd38ea185e73366f4ba65a5c
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-01 11:28:54 +0530

    changes added

--- core-java/src/com/shreya/practice/oops/abstraction/animal/TestAbstraction.java
+++ core-java/src/com/shreya/practice/oops/abstraction/animal/TestAbstraction.java
@@ -1,22 +1,24 @@
 package com.shreya.practice.oops.abstraction.animal;
 
- abstract class Animal {
-     abstract void sound();   //Abstract method nobody
+abstract class Animal {
+    abstract void sound();   //Abstract method nobody
 
-     void sleep(){            // non-abstract method
-         System.out.println("Animal sleeping..");
-     }
-}
-class Dog extends  Animal{
-
-     void sound() {
-         System.out.println("Dog barks");
+    void sleep() {            // non-abstract method
+        System.out.println("Animal sleeping..");
     }
 }
- public class TestAbstraction {
-     public static void main(String[] args) {
-         Animal obj = new Dog();
-         obj.sound();
-         obj.sleep();
-     }
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+        Animal obj = new Dog();
+        obj.sound();
+        obj.sleep();
+    }
 }
