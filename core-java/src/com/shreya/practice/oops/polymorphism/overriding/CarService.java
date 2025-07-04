8ba838eed25e0e6de782497f61dfd978ee5a3ab7
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-04 09:52:15 +0530

    changes added

--- core-java/src/com/shreya/practice/oops/polymorphism/overriding/CarService.java
+++ core-java/src/com/shreya/practice/oops/polymorphism/overriding/CarService.java
@@ -3,5 +3,6 @@ package com.shreya.practice.oops.polymorphism.overriding;
 public class CarService extends VehicleService {
     public void move() {
         System.out.println("Car is moving...");
+
     }
 }
