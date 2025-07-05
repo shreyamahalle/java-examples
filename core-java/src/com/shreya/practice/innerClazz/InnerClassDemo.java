11da4a9ec600b486236e48bb54121141e7dcd90e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-05 17:15:05 +0530

    inner class example added

---
+++ core-java/src/com/shreya/practice/innerClazz/InnerClassDemo.java
@@ -0,0 +1,22 @@
+package com.shreya.practice.innerClazz;
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine("1.0 ltr turbo " , "petrol");
+        Car car = new Car(200 , "BMW" , "xse300",engine);
+
+        System.out.println("Car Details " + car);
+
+        printCarDetails(car);
+        printCEngineDetails(car.engine);
+
+    }
+
+    private  static void printCarDetails(Car car){
+        System.out.println("Car Details " + car);
+    }
+    private  static void printCEngineDetails(Car.Engine engine){
+        System.out.println("Car Details " + engine);
+
+    }
+}
