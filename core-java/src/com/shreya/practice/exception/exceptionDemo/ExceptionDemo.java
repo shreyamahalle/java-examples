68412c1b18fd3ec1784be021b32c0b4f42240d02
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-07 19:13:14 +0530

    changes added'

--- core-java/src/com/shreya/practice/exception/exceptionDemo/ExceptionDemo.java
+++ core-java/src/com/shreya/practice/exception/exceptionDemo/ExceptionDemo.java
@@ -3,9 +3,19 @@ package com.shreya.practice.exception.exceptionDemo;
 import java.util.Arrays;
 
 public class ExceptionDemo {
-    public static void main(String[] args) {
-        int[] marks = new int[5];
 
+    public static void main(String[] args) {
+        processMarksUsingTryCatch();
+
+        try {
+            processMarksUsingThrows();
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+    private static void processMarksUsingTryCatch() {
+
+        int[] marks = new int[5];
         marks[0] = 99;
         marks[1] = 86;
         marks[2] = 89;
@@ -14,11 +24,26 @@ public class ExceptionDemo {
         try {
             marks[5] = 90;
             System.out.println("marks [5] data completed");
-        } catch (Exception exception) {
+        } catch (ArrayIndexOutOfBoundsException exception) {
             System.err.println("you are doing something wrong.we have only 5 subjects." + exception.getMessage());
-
         }
+        System.out.println("marks of the students is " + Arrays.toString(marks));
+    }
 
+    private static void processMarksUsingThrows()throws Exception {
+
+        int[] marks = new int[5];
+        marks[0] = 99;
+        marks[1] = 86;
+        marks[2] = 89;
+        marks[3] = 67;
+        marks[4] = 90;
+        try {
+            marks[5] = 90;
+            System.out.println("marks [5] data completed");
+        } catch (ArrayIndexOutOfBoundsException exception) {
+                 throw  new Exception("Why you are inserting marks of 6 subject ??");
+        }
         System.out.println("marks of the students is " + Arrays.toString(marks));
     }
 }
