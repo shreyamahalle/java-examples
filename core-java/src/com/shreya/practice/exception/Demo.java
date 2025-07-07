bd9754a626e71fb51adea83464ca28353809019a
shreya mahalle <shreyamahalle2000@gmail.com>
2025-07-07 22:04:45 +0530

    changes added

---
+++ core-java/src/com/shreya/practice/exception/Demo.java
@@ -0,0 +1,15 @@
+package com.shreya.practice.exception;
+
+public class Demo {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 5;
+            System.out.println("Result " + result);
+        } catch (RuntimeException e) {
+            System.out.println("Exception occurred " + e.getMessage());
+            throw new RuntimeException(e);
+        }finally {
+            System.out.println("this will always execute");
+        }
+    }
+}
