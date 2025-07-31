f6bc197abdd8df988b359e3b39e7d2da1eba4d60
shreya <shreyamahalle2000@gmail.com>
2025-07-31 16:31:48 +0530

    updated code

---
+++ data-types-examples/src/addition/ValueTwo.java
@@ -0,0 +1,13 @@
+package addition;
+//2 variables
+//perform the addition operators? use operator
+//1 variable for storing in memory
+//print the output
+
+public class ValueTwo {
+    public static void main(String[] args) {
+        int a = 10 , b = 20;
+        int c = a + b;
+        System.out.println("sun is " + c );
+    }
+}
