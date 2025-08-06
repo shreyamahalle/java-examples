563b3a8b5a34ebc14ae973709533e0ad490337df
shreya <shreyamahalle2000@gmail.com>
2025-08-06 11:02:02 +0530

    changes added

--- operators-demo/src/com/operators/relational/Number.java
+++ operators-demo/src/com/operators/relational/Number.java
@@ -6,7 +6,7 @@ public class Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        if(n % 2 ==0){
+        if(n > 0){
             System.out.println("number is even");
         }else {
             System.out.println("no is odd");
