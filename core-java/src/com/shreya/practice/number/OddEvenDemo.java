cc6f8ecd9e5ee2ef92cf98f8f5a7ceaa1fd5b010
shreya <shreyamahalle2000@gmail.com>
2025-08-06 11:52:00 +0530

    added

--- core-java/src/com/shreya/practice/number/OddEvenDemo.java
+++ core-java/src/com/shreya/practice/number/OddEvenDemo.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class OddEvenDemo {
 
     private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.println("Enter number");
         int no = sc.nextInt();
