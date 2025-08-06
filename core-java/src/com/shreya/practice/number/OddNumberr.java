411966ffd2063076b0b9608cde5499dc92d28202
shreya <shreyamahalle2000@gmail.com>
2025-08-06 11:37:07 +0530

    code added

---
+++ core-java/src/com/shreya/practice/number/OddNumberr.java
@@ -0,0 +1,16 @@
+package com.shreya.practice.number;
+
+import java.util.Scanner;
+
+public class OddNumberr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no to check odd or even");
+        int no = Integer.parseInt(sc.nextLine());
+        if(no % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
