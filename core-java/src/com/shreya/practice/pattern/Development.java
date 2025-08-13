52b7865c4f9ecfe54d6efd3b8c61aabd1a492f74
shreya <shreyamahalle2000@gmail.com>
2025-08-13 12:36:23 +0530

    pattern code added

---
+++ core-java/src/com/shreya/practice/pattern/Development.java
@@ -0,0 +1,21 @@
+package com.shreya.practice.pattern;
+//*
+//**
+//***
+//****
+//*****
+
+public class Development {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5){
+            int j = 0;
+            while (j <= i) {
+                System.out.print(" *");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
